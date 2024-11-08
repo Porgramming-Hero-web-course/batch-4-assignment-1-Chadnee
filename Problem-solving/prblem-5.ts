6485219e1a3276f57b4bcea9a77af1614fdb20fc
Mstchadnee <116938357+Mstchadnee@users.noreply.github.com>
2024-11-08 11:22:58 +0600

    5th problem solved

--- Problem-solving/prblem-5.ts
+++ Problem-solving/prblem-5.ts
@@ -1,2 +1,17 @@
 //problem-5
 
+//generic function using constraint - using keyof
+const getProperty = <X,Y extends keyof X>(obj:X, key:Y) =>{
+   
+ // return obj[key]
+   console.log(obj[key])
+
+
+}
+
+//with sample input
+
+const person = {name:"Alice", age:30};
+
+//call the function
+getProperty(person, "name");
